9e34571b005da72fabd0f8ca93c6132a877877a9
Kevin Manzi <k.manzi@alustudent.com>
2025-11-21 12:57:15 +0000

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+This module exports all tasks owned by a specific user into a JSON file.
+
+The JSON file format must be:
+{
+    "USER_ID": [
+        {
+            "task": "TASK_TITLE",
+            "completed": TASK_COMPLETED_STATUS,
+            "username": "USERNAME"
+        },
+        ...
+    ]
+}
+
+The file name is: USER_ID.json
+"""
+
 import json
 import requests
 import sys
@@ -7,35 +25,37 @@ import sys
 if __name__ == "__main__":
     user_id = int(sys.argv[1])
 
-    # URLs
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    # Build URLs
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # Fetch data
-    todo_data = requests.get(todos_url).json()
+    # Fetch user data
     user_data = requests.get(user_url).json()
-
     username = user_data.get("username")
 
-    # Build list of task dictionaries
+    # Fetch all todos
+    todos = requests.get(todos_url).json()
+
+    # Create list of task dictionaries
     tasks_list = []
-    for todo in todo_data:
+    for todo in todos:
         if todo.get("userId") == user_id:
-            task_dict = {
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": username
-            }
-            tasks_list.append(task_dict)
+            tasks_list.append(
+                {
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed"),
+                    "username": username
+                }
+            )
 
-    # Final JSON structure
-    output = {str(user_id): tasks_list}
+    # Structure for JSON export
+    json_data = {str(user_id): tasks_list}
 
-    # Filename
+    # File name
     file_name = "{}.json".format(user_id)
 
     # Write to JSON file
-    with open(file_name, "w", encoding="utf-8") as json_file:
-        json.dump(output, json_file)
+    with open(file_name, "w", encoding="utf-8") as f:
+        json.dump(json_data, f)
 
-    print("JSON file '{}' has been created.".format(file_name))
+    print("JSON file '{}' created.".format(file_name))
